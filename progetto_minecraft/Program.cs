cc3b6a681da5b055757b243de902ee3b7f0b3715
Christian-Di-Bello <dibello.christian.21@itisriva.edu.it>
2025-05-25 23:13:34 +0200

    inizializzazione costanti

---
+++ progetto_minecraft/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace progetto_minecraft
+{
+    internal class Program
+    {
+        static string[] materialiDisponibili = { "Legno", "Pietra", "Ferro", "Oro", "Carbone", "Diamante" };
+        static string[] materialiInventario = new string[4];
+        static int[] quantitaInventario = new int[4];
+        static Random generatoreCasuale = new Random();
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
